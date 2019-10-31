96f8d97eedd62ccd92ee187049cdbc4ed5e15ccc
LauraHantz <laura.hantz@hotmail.fr>
2019-10-31 16:57:28 +0100

    travailler dans books.php (views) et index.php (public)

--- views/books.php
+++ views/books.php
@@ -1,15 +1,20 @@
 <?php $title = "Liste des livres"; ?>
 <?php ob_start(); ?>
-<h1>Liste des livres</h1>
 
-<ul>
-  <?php
-    foreach ($books as $book) {
-      echo '<li>' . $book['title'] . '</li>';
-    }
-  ?>
+<div class="container">
 
-</ul>
+
+  <h1>Liste des livres</h1>
+
+  <ul>
+    <?php
+      foreach ($books as $book) {
+        echo '<li>' . $book['title'] . '</li>';
+      }
+    ?>
+
+  </ul>
+</div>
 
 
 <?php $content = ob_get_clean(); ?>
