ebdea4ee878586d61434388fe1feace25ca2a03d
LauraHantz <laura.hantz@hotmail.fr>
2019-10-31 15:47:13 +0100

    Mise à jour mise en route

--- views/books.php
+++ views/books.php
@@ -2,5 +2,6 @@
 <?php ob_start(); ?>
 <h1>Liste des livres</h1>
 <p>Bienvenue sur la page de la liste des livres</p>
+<?php var_dump($books); ?>
 <?php $content = ob_get_clean(); ?>
 <?php require('public/index.php'); ?>
