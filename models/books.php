eae73ee083cf280f3542a9716d9106be87789145
LauraHantz <laura.hantz@hotmail.fr>
2019-11-05 12:07:16 +0100

    fichiers à jours

--- models/books.php
+++ models/books.php
@@ -1,11 +1,20 @@
 <?php
-
+require_once('utils/db.php');
 
 function getBooks()
 {
-    $file = file_get_contents('json/books.json');
-    $books = json_decode($file, true);
-    return $books;
+
+    $db = dbConnect();
+
+    $stmt = $db->prepare('SELECT * FROM books');
+
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // $file = file_get_contents('json/books.json');
+    // $books = json_decode($file, true);
+    // return $books;
 
 }
 
